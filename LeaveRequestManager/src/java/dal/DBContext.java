980f1c1638f7d8f02e1a309c612c92350fbceb10
luongnd2625 <luongnd2625@gmail.com>
2025-10-17 17:49:33 +0700

    Code Dbcontext.java
    This class will manage db, and I've create d gerConnection() func to have Connection, to use.
    (And write simple code to check getConnection work !!)

--- LeaveRequestManager/src/java/dal/DBContext.java
+++ LeaveRequestManager/src/java/dal/DBContext.java
@@ -6,17 +6,49 @@ import java.sql.SQLException;
 
 //Connect to db
 public class DBContext {
+    
+// SQL Server Connection
+    public Connection GetConnection() throws Exception {
+        
+        //Connection Settings
+        String serverName = "Ber"; //Instance Name
+        String dbName = "LeaveRequestManager"; //DB
+        String portNumber = "1433"; //Portt
+        String instance = ""; // Nếu SQL Server có instance riêng thì ghi ở đây, VD: "Ber\\SQLEXPRESS"
+        String userID = "luongnd"; 
+        String password = "12345";
 
-    private final String serverName = "localhost\\Ber"; // Instance name
-    private final String dbName = "LeaveRequestManager";
-    private final String user = "luongnd";
-    private final String pass = "12345";
+        // Chuỗi kết nối
+        String connectionURL = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";"
+                + "databaseName=" + dbName + ";encrypt=false;";
 
-    public Connection getConnection() throws SQLException {
-        String connectionUrl = "jdbc:sqlserver://" + serverName
-                + ";databaseName=" + dbName
-                + ";encrypt=false;user=" + user
-                + ";password=" + pass + ";";
-        return DriverManager.getConnection(connectionUrl);
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(connectionURL, userID, password);
+        } catch (SQLException e) {
+            System.err.println("Lỗi kết nối SQL Server: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    // Hàm main chỉ để test kết nối riêng
+    public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            DBContext db = new DBContext();
+            conn = db.GetConnection();
+            if (conn != null) {
+                System.out.println("Kết nối thành công!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
+
